98234ae3395719f776feb81122fc44156fd43205
Ammar-Satouf <viberus512@gmail.com>
2025-05-23 19:51:15 +0300

    Create resources.py

---
+++ resources.py
@@ -0,0 +1,24 @@
+resources = {
+    "1": {
+        "1": {
+            "practical": {
+                "programming1": {
+                    "lectures": "https://drive.google.com/programming1_lectures",
+                    "exams": "https://drive.google.com/programming1_exams",
+                    "notes": "ركز على أساسيات C++ والخوارزميات."
+                },
+                # ... أكمل باقي المواد بنفس الطريقة
+            },
+            "theoretical": {
+                "arabic": {
+                    "lectures": "https://drive.google.com/arabic_lectures",
+                    "exams": "https://drive.google.com/arabic_exams",
+                    "notes": "ركز على القواعد والنحو."
+                },
+                # ... أكمل باقي المواد
+            },
+        },
+        # ... الفصل 2 لو حابب تضيفه
+    },
+    # ... السنة 2 لو حابب تضيفها
+}
